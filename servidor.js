229d3b5da3de7116f4e92f278e606362f5f195e1
Erick Aranibar <aranibarerick@gmail.com>
2019-10-13 21:39:21 -0400

    socket

--- servidor.js
+++ servidor.js
@@ -18,7 +18,7 @@ rutas.post('/', (req, res)=>{
   var respuesta = "Cantidad: " + cant.toString() + " Puerto: " + puerto.toString();
 
   io.emit("Cambios",respuesta)
-  console.log("Llegó:",req);
+  console.log("Llegó:",req.query);
 	//io.sockets.emit('transmit', { msg: req.query});
 });
 /*
